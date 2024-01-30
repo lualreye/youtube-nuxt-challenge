0b2772ed4a4a03585b57194983771568db6188ff
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2024-01-30 12:35:07 -0500

    FIX: cors politics resolve ssr disabled

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath } from 'url';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
