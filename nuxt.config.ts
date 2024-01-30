f1799f577dba510d6af7d66a70516376339ae78b
Luis Alberto Reyes Lema <lualreye@Luiss-MacBook-Pro.local>
2024-01-30 13:04:44 -0500

    RM: ssr in false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath } from 'url';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
